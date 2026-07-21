138fe114ca49d21a76f5a6676779dfa5f2646848
CeloHT <celoht3@gmail.com>
2026-07-21 15:57:47 -0400

    Create page.tsx

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,10 @@
+import BlogList from "@/components/sections/BlogList";
+
+export default function BlogPage() {
+  return (
+    <section className="container mx-auto py-16">
+      <h1 className="text-4xl font-bold mb-6">Blog</h1>
+      <BlogList />
+    </section>
+  );
+}

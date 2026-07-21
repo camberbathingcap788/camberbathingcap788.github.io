868fa3b62796846b7a0d88c2a6644b301bf1cafa
CeloHT <celoht3@gmail.com>
2026-07-21 15:43:48 -0400

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import HeroSection from "@/components/sections/HeroSection";
+import PillarsSection from "@/components/sections/PillarsSection";
+import ImpactPreview from "@/components/sections/ImpactPreview";
+import PartnersPreview from "@/components/sections/PartnersPreview";
+
+export default function HomePage() {
+  return (
+    <>
+      <HeroSection />
+      <PillarsSection />
+      <ImpactPreview />
+      <PartnersPreview />
+    </>
+  );
+}

3dead447a6c708c8dc478f5cf8214f86471ad3de
CeloHT <celoht3@gmail.com>
2026-07-21 15:48:14 -0400

    Create manifest.ts

---
+++ src/app/manifest.ts
@@ -0,0 +1,24 @@
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "CeloHT",
+    short_name: "CeloHT",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#00a884",
+    icons: [
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png"
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png"
+      }
+    ]
+  };
+}
